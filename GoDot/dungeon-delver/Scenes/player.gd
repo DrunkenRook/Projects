7ce83e000e5db5671631aacb181df097c830d96e
DrunkenRook <mpkelly101@gmail.com>
2025-11-24 11:11:49 -0500

    Sidescroller game added

--- GoDot/dungeon-delver/Scenes/player.gd
+++ GoDot/dungeon-delver/Scenes/player.gd
@@ -1,54 +1,31 @@
-#Player sprite assets aquired from https://penusbmic.itch.io/sci-fi-character-pack-10
-# Script written with the help of GoDot's beginner tutorial
-
-
 extends CharacterBody2D
 
-
-var speed = 500
+@export var speed = 400
 var screen_size
-
 
 func ready():
 	screen_size = get_viewport().size
-	$PlayerAnimation.play(&"Idle", true)
 
 func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("moveRight"):
-		velocity.x += 20
+		velocity.x += 1
 	if Input.is_action_pressed("moveLeft"):
-		velocity.x -= 20
+		velocity.x -= 1
 	if Input.is_action_pressed("moveDown"):
-		velocity.y += 20
+		velocity.y += 1
 	if Input.is_action_pressed("moveUp"):
-		velocity.y -= 20
-	if Input.is_action_pressed("attack"):
-		$PlayerAnimation.animation = &"Attack"
-		#while $PlayerAnimation.animation == &"Attack":
-			#velocity.x = 0
-			#velocity.y = 0
-			
-			
+		velocity.y -= 1
 	
-	if velocity.x != 0 or velocity.y != 0:
-		$PlayerAnimation.animation = &"Run"
+	if velocity.x != 0:
+		$PlayerAnimation.animation = "Run"
+		$PlayerAnimation.flip_v = false
 		$PlayerAnimation.flip_h = velocity.x < 0
-	if velocity.x == 0 and velocity.y == 0 and $PlayerAnimation.animation != &"Attack":
-		$PlayerAnimation.animation = &"Idle"
+	#elif velocity.y != 0:
+		#$PlayerAnimation.animation = "Up"
+		#$AnimatedSprite2D.flip_v = velocity.y > 0
+	else:
+		$PlayerAnimation.animation = "Idle"
 	
 	position += velocity * delta
-	
-	
-func on_animation_finished() -> void:
-	$PlayerAnimation.animation = &"Idle"
-	
-func on_animation_looped() -> void:
-	$PlayerAnimation.animation = &"Idle"
-<<<<<<< HEAD
-	
-func on_attack_entered(area):
-	if area.is_in_group("hitbox"):
-		area.take_damage()
-=======
->>>>>>> c59fe612910da9adeb5bf034e50d7c92fb35f690
+	#position = position.clamp(Vector2.ZERO, screen_size)
